3740630c7f236b1ca32fcc3376d1fc5f53eccc66
Ben <bsto130@wgu.edu>
2026-05-06 18:02:48 -0500

    feat: extract footer into shared components/footer.js

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+document.currentScript.insertAdjacentHTML('afterend', `
+  <footer class="footer">
+    <p class="footer__logo">Mahjong Social at Solara</p>
+    <p class="footer__tagline"><em>Play beautifully.</em></p>
+    <nav class="footer__links">
+      <a href="index.html">Home</a>
+      <a href="booking.html">Booking</a>
+      <a href="location.html">Location</a>
+      <a href="about.html">About</a>
+      <a href="contact.html">Contact</a>
+    </nav>
+    <p class="footer__copy">&copy; 2026 Mahjong Social at Solara. All rights reserved.</p>
+  </footer>
+`);
