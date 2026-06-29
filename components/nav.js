b5a315e86c2bcc3120dffdbafd0e196497c07852
BenTheWin <benstone2004@gmail.com>
2026-06-29 17:10:55 -0500

    feat: add Travel & Tiles to nav

--- components/nav.js
+++ components/nav.js
@@ -8,6 +8,7 @@
         '<li><a href="booking.html">Booking</a></li>' +
         '<li><a href="location.html">Location</a></li>' +
         '<li><a href="about.html">About</a></li>' +
+        '<li><a href="travel-tiles.html">Travel &amp; Tiles</a></li>' +
         '<li><a href="faq.html">FAQ</a></li>' +
         '<li><a href="contact.html">Contact</a></li>' +
       '</ul>' +
